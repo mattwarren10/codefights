adaed444094362a83a5e1c294c3880a8795e3ea3
Matt Warren <mwarren@harding.edu>
2017-11-16 11:16:18 -0500

    create binary search

--- first_duplicate.rb
+++ first_duplicate.rb
@@ -1,16 +1,36 @@
-a = [2, 3, 3, 1, 5, 2]
+a = [2, 3, 3]
 
 def firstDuplicate(a)  
   dup_idc = []
-  a.each_with_index do |n, i|
-    (i+1..a.length-1).each do |o|
-      if n == a[o]
+  l = (a.length/2).floor
+  b = a[0..l]
+  c = a[l+1..a.length-1]
+  b.each_with_index do |n, i|
+    (i+1..b.length-1).each do |o|
+      if n == b[o]
         dup_idc << o
         break o
       end
     end
   end
+  if dup_idc.empty?
+  	c.each_with_index do |n, i|  		
+    	(i+1..c.length).each do |o|
+    		p n
+    		p i
+      	if n == b[i] || n == c[o]
+        	dup_idc << o+l
+        	break o
+      	end
+    	end
+  	end
+  end
   dup_idc.empty? ? -1 : a[dup_idc.min]
 end
 
 puts firstDuplicate(a)
+
+# b = [2, 3]; c = [3]
+
+# first iteration on c
+# n = 3, c[o] = 3, b[i] = 
