ee868382d176407ecad8908f9ef381ad90c6d2af
Matt Warren <mwarren@harding.edu>
2017-11-16 08:26:15 -0500

    change dup_idx to arr

--- first_duplicate.rb
+++ first_duplicate.rb
@@ -1,15 +1,16 @@
 a = [2, 3, 3, 1, 5, 2]
 
-def firstDuplicate(a)  
-  dup_idx = a.length
+def first_duplicate(a)  
+  dup_idc = []
   a.each_with_index do |n, i|
     (i+1..a.length-1).each do |o|
       if n == a[o]
-        o < dup_idx ? dup_idx = o : dup_idx
+        dup_idc << o
+        break o
       end
     end
   end
-  dup_idx == a.length ? -1 : a[dup_idx]
+  dup_idc.empty? ? -1 : a[dup_idc.min]
 end
 
-puts firstDuplicate(a)
+puts first_duplicate(a)
