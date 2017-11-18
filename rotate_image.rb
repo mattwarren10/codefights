dc64e3e2e7e6527fb5d0e40ab24dd46aaf913b55
Matt Warren <mwarren@harding.edu>
2017-11-18 09:53:26 -0500

    return rotated image

--- rotate_image.rb
+++ rotate_image.rb
@@ -1,15 +1,23 @@
 def rotateImage(a)
 	b = []
-	a.each do |n|
-		
+	(a.length).times do |i|
+		n = a.length - 1
+		b << []
+		(a.length).times do |j|
+			b[i][j] = a[n][i]
+			n -= 1
+		end
 	end
+	b
 end
+
+
 
 a = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
 
-puts rotateImage(a)
+p rotateImage(a)
 
 # rotate 90 degrees clockwise
 # output should be 
@@ -20,16 +28,15 @@ puts rotateImage(a)
 #      [9, 6, 3]]
 
 # 3 arrays in one array
-# a[0][0] -> a[0][2]
-# a[0][1] -> a[1][2]
-# a[0][2] -> a[2][2]
 
-# a[1][0] -> a[0][1]
-# a[1][1] -> a[1][1]
-# a[1][2] -> a[2][1]
+# b[0][0] = a[2][0]
+# b[0][1] = a[1][0]
+# b[0][2] = a[0][0]
 
-# a[2][0] -> a[0][0]
-# a[2][1] -> a[1][0]
-# a[2][2] -> a[2][0]
+# b[1][0] = a[2][1]
+# b[1][1] = a[1][1]
+# b[1][2] = a[0][1]
 
-
+# b[2][0] = a[2][2]
+# b[2][1] = a[1][2]
+# b[2][2] = a[0][2]
