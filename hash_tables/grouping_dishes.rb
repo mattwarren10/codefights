addc257695002b7c7a4066e63c8518def2ee3de9
Matt Warren <mwarren@harding.edu>
2017-11-20 03:16:53 -0500

    add hash_tables

---
+++ hash_tables/grouping_dishes.rb
@@ -0,0 +1,10 @@
+def groupingDishes(dishes)
+
+end
+
+dishes = [	["Salad", "Tomato", "Cucumber", "Salad", "Sauce"],
+            ["Pizza", "Tomato", "Sausage", "Sauce", "Dough"],
+            ["Quesadilla", "Chicken", "Cheese", "Sauce"],
+            ["Sandwich", "Salad", "Bread", "Tomato", "Cheese"]]
+
+p groupingDishes(dishes)
